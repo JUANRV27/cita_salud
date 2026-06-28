c3fc25da7bdcc33b6e54b999d1a04a124c47c1f2
JUANRV27 <rodriguezvelasquezjuanjose11@gmail.com>
2026-06-28 15:17:25 -0500

    Configuración Docker

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from config import Config
@@ -62,5 +63,8 @@ app.register_blueprint(
 )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(        
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
 
